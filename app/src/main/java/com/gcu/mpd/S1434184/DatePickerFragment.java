1254a84f9a8a5537d73a6e6300ad9f96a5b42b26
Niklas Olsson <niklasolsson94@gmail.com>
2019-04-10 02:15:21 +0100

    Created release APK

--- app/src/main/java/com/gcu/mpd/S1434184/DatePickerFragment.java
+++ app/src/main/java/com/gcu/mpd/S1434184/DatePickerFragment.java
@@ -47,6 +47,7 @@ public class DatePickerFragment extends DialogFragment
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
+
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
